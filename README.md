23d23f95161f23f0c51102ba227ca8c57e8adfb4
Sthefanyailez <ailezsthefany@gmail.com>
2024-10-10 22:12:24 -0300

    Update and rename README.md to src/routes/home/index.js
    adicionar arquivo index.js para a rota home

--- README.md
+++
@@ -1,2 +0,0 @@
-# ATIVIDADE1
-Repositório para a atividade 1 de Programação Mobile 
---
+++ src/routes/home/index.js
@@ -0,0 +1,4 @@
+# ATIVIDADE1
+Repositório para a atividade 1 de Programação Mobile 
+const RA = "Seu RA aqui"; // Substitua pelo seu RA   
+console.log(RA);
